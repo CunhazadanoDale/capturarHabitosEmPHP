a49ad29627638956146c14d9c86a7cac06246810
CunhazadaNoDale <cunha12324@gmail.com>
2026-02-23 23:02:40 -0300

    feat: aprendendo rotas em php que chamam os endpoints

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SiteController;
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Route::get('/hello/{name}', function ($name) {
+    return "Hello, $name!";
+});
+
+Route::get('/', [SiteController::class, 'index']);
